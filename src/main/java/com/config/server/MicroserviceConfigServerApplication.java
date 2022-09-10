26bc8365431dff2f4723f7cc107cdd2600d50525
parmeshwar <parmeshwar92@gmail.com>
2022-09-10 23:16:52 +0530

    first commit

---
+++ src/main/java/com/config/server/MicroserviceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+	}
+
+}
